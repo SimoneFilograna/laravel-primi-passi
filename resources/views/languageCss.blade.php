137f0711764be5d19d6a56fb0875824dce338984
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-09-24 14:32:24 +0200

    add-style

--- resources/views/languageCss.blade.php
+++ resources/views/languageCss.blade.php
@@ -7,17 +7,17 @@
     <title>CSS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="/resources/css/home.css">
+    <link rel="stylesheet" href="{{URL::asset('css/style.css')}}">
 </head>
 <body>
     <header>
-        <div class="container text-center mt-5">
-            <h1 class="dispay-1">{{$css["name"]}}</h1>
+        <div class="container text-center mt-5 text-white">
+            <h1 class="display-1">{{$css["name"]}}</h1>
         </div>
     </header>
     <main>
         <div class="container mt-5 mb-4">
-            <div class="box text-center">
+            <div class="box p-5 text-center bg-white rounded-5">
                 <img src="{{$css["img"]}}" alt="" class="">
                 <p class="fs-5 pt-5">{{$css["description"]}}</p>
                 <a href="/" class="btn btn-primary mt-3">Torna Indietro</a>
